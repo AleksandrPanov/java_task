fff53de20aebb5ccd0be49d77e1789b64e699ad1
Alexander <sasha1_1997@mail.ru>
2017-10-14 23:53:36 +0300

    complex num

---
+++ task2/src/com/company/MyComplex.java
@@ -0,0 +1,83 @@
+package com.company;
+
+public class MyComplex {
+    private double real = 0.0;
+    private double imag = 0.0;
+
+    public MyComplex() {
+    }
+
+    public MyComplex(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImag() {
+        return imag;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public void setImag(double imag) {
+        this.imag = imag;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + real +"+" + imag + "i)";
+    }
+
+    void SetValue(double real, double imag)
+    {
+
+        this.real = real;
+        this.imag = imag;
+    }
+
+    boolean isReal()
+    {
+        return (imag == 0.0);
+    }
+    boolean isImaginary()
+    {
+        return (real == 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MyComplex myComplex = (MyComplex) o;
+
+        if (Double.compare(myComplex.real, real) != 0) return false;
+        return Double.compare(myComplex.imag, imag) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(real);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(imag);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+    double magnitude()
+    {
+        return Math.sqrt(real*real + imag*imag);
+    }
+    double argument()
+    {
+        if (real == 0 && imag == 0) return -100.0;
+        return 0.0;
+    }
+
+}
