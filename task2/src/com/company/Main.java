5cc617ecfb9cf867e3b9902fac06a78740728039
Alexander <Sasha1_1997@mail.ru>
2017-10-18 21:32:43 +0400

    added polynomial

--- task2/src/com/company/Main.java
+++ task2/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MyPolynomial pol1 = new MyPolynomial(1,2,3); // 1 + 2x + 3x^2
+        MyPolynomial pol2 = new MyPolynomial(1,-2,3,4); // 1 -2x + 3x^2 + 4x^3
+        MyPolynomial pol3 = new MyPolynomial(2,3);
+        MyPolynomial pol4 = new MyPolynomial(-2,3);
+        System.out.println(pol1);
+        System.out.println(pol1.add(pol2));
+        System.out.println(pol2.add(pol1));
+        System.out.println(pol4.multiply(pol3));
+        System.out.println(pol3.multiply(pol1));
     }
 }
--- task2/src/com/company/MyPolynomial.java
+++ task2/src/com/company/MyPolynomial.java
@@ -5,28 +5,72 @@ import java.util.Arrays;
 public class MyPolynomial {
     private double[] coef;
 
-    public MyPolynomial(double coef) {
-
+    public MyPolynomial(double ... coef) {
+        this.coef = coef.clone();
     }
     int getDegree()
     {
-        return coef.length;
+        return coef.length - 1;
     }
-
     @Override
     public String toString() {
-        return "MyPolynomial{" +
-                "coef=" + Arrays.toString(coef) +
-                '}';
+        String s = String.valueOf(coef[0]);
+        String delimiter;
+        for (int i = 1; i < coef.length; i++)
+        {
+            if (coef[i-1] >= 0) delimiter = "+";
+            else delimiter = "";
+            s = String.join(delimiter,String.valueOf(coef[i]) + "x^" + String.valueOf(i), s);
+        }
+        return s;
     }
 
     MyPolynomial add(MyPolynomial o)
     {
-        return new MyPolynomial(0.0);
+        MyPolynomial arMax = new MyPolynomial();
+        if (this.coef.length >= o.coef.length)
+        {
+            arMax.coef = new double[this.coef.length];
+            for (int i = 0; i < o.coef.length; i++)
+                arMax.coef[i] = coef[i] + o.coef[i];
+            for(int i = o.coef.length; i < arMax.coef.length; i++)
+                arMax.coef[i] = coef[i];
+        }
+        else
+        {
+            arMax.coef = new double[o.coef.length];
+            for (int i = 0; i < this.coef.length; i++)
+                arMax.coef[i] = coef[i] + o.coef[i];
+            for(int i = this.coef.length; i < arMax.coef.length; i++)
+                arMax.coef[i] = o.coef[i];
+        }
+        return arMax;
+    }
+    MyPolynomial multiply(MyPolynomial o) //not effective - O(n^2), but very easy
+    {
+        MyPolynomial pol = new MyPolynomial();
+        for (int i = 0; i < o.coef.length; i++)
+            pol = pol.add(multiply(o.coef[i], i));
+        return pol;
+    }
+    MyPolynomial multiply(double coef, int pow)
+    {
+        MyPolynomial pol = new MyPolynomial();
+        pol.coef = new double[this.coef.length + pow];
+        for (int i = pow; i < pol.coef.length; i++)
+            pol.coef[i] = this.coef[i - pow] * coef;
+        return pol;
     }
 
-    MyPolynomial multiplyy(MyPolynomial o)
+    double evaluate(double x)
     {
-        return new MyPolynomial(0.0);
+        double res = 0;
+        double xi = 1;
+        for (int i = 0; i < coef.length; i++)
+        {
+            res += coef[i]*xi;
+            xi *= x;
+        }
+        return res;
     }
 }
