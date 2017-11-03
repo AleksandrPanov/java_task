20c30dda0f70b00fd5dcb069c950e27ddf91b2a7
Alexander <Sasha1_1997@mail.ru>
2017-11-03 18:49:09 +0300

    add task4.2

--- task4/src/com/company/Main.java
+++ task4/src/com/company/Main.java
@@ -1,14 +1,62 @@
 package com.company;
-import java.util.ArrayList;
-import java.util.LinkedList;
-public class Main {
+import java.util.*;
 
+public class Main {
+    public static void task2A() {
+        int n = 1000000;
+        long startTime ;
+        long estimatedTime;
+        int ar[] = GenArray.getRandAr(n, -1000, 1000);
+        ArrayList<Integer> c1 = new ArrayList<>();
+        LinkedList<Integer> c2 = new LinkedList<>();
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < n; i++)
+            c1.add(ar[i]);
+        estimatedTime = System.nanoTime() - startTime;
+        System.out.println(estimatedTime/1000000);
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < n; i++)
+            c2.add(ar[i]);
+        estimatedTime = System.nanoTime() - startTime;
+        System.out.println(estimatedTime/1000000);
+    }
+    public static void task2B() {
+        int n = 10000;
+        int ar[] = GenArray.getRandAr(n, -1000, 1000);
+        HashSet<Integer> c3 = new HashSet<>();
+        LinkedHashSet<Integer> c4 = new LinkedHashSet<>();
+        TreeSet<Integer> c5 = new TreeSet<Integer>();
+
+        for (int i = 0; i < n; i++)
+            c3.add(ar[i]);
+
+        for (int i = 0; i < n; i++)
+            c4.add(ar[i]);
+
+        for (int i = 0; i < n; i++)
+            c5.add(ar[i]);
+    }
+    public static void task2C() {
+        int n = 10000;
+        int ar[] = GenArray.getRandAr(n, -1000, 1000);
+        HashMap<Integer, Integer> c6 = new HashMap<Integer, Integer>();
+        LinkedHashMap<Integer, Integer> c7 = new  LinkedHashMap<Integer, Integer>();
+        TreeMap<Integer, Integer> c8 = new TreeMap<Integer, Integer>();
+
+        for (int i = 0; i < n/2; i++)
+            c6.put(ar[i], ar[i + n/2]);
+
+        for (int i = 0; i < n/2; i++)
+            c7.put(ar[i], ar[i + n/2]);
+
+        for (int i = 0; i < n/2; i++)
+            c8.put(ar[i], ar[i + n/2]);
+    }
     public static void main(String[] args) {
-	// write your code here
-        ArrayList<Integer> ar = new ArrayList<Integer>();
-        ar.add(10);
-        ar.add(11);
-        LinkedList<Integer> l = new LinkedList<>();
-        System.out.println(ar.get(0));
+	task2A();
+	task2B();
+	task2C();
     }
 }
